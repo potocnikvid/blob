63028958a1d8dba72dfa8358930aaf96db967c1c
potocnikvid <vidpotocnik0@gmail.com>
2023-11-29 10:44:05 +0100

    pose detection working

--- js/pose.js
+++ js/pose.js
@@ -1,111 +1,109 @@
-"use strict";
-// Copyright 2023 The MediaPipe Authors.
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
+const video5 = document.getElementsByClassName("input_video5")[0];
+const out5 = document.getElementsByClassName("output5")[0];
+const controlsElement5 = document.getElementsByClassName("control5")[0];
+const canvasCtx5 = out5.getContext("2d");
+
+const fpsControl = new FPS();
+
+const spinner = document.querySelector(".loading");
+spinner.ontransitionend = () => {
+  spinner.style.display = "none";
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-import("@mediapipe/tasks-vision").then(({ FilesetResolver, DrawingUtils, PoseLandmarker }) => {
-    const demosSection = document.getElementById("demos");
-    let poseLandmarker;
-    let webcamRunning = false;
-    const videoHeight = "360px";
-    const videoWidth = "480px";
-    // Before we can use PoseLandmarker class we must wait for it to finish
-    // loading. Machine Learning models can be large and take a moment to
-    // get everything needed to run.
-    function createPoseLandmarker() {
-        return __awaiter(this, void 0, void 0, function* () {
-            const vision = yield FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm");
-            poseLandmarker = yield PoseLandmarker.createFromOptions(vision, {
-                baseOptions: {
-                    modelAssetPath: `https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.task`,
-                    delegate: "GPU",
-                },
-                runningMode: "VIDEO",
-                numPoses: 2,
-            });
-            demosSection === null || demosSection === void 0 ? void 0 : demosSection.classList.remove("invisible");
-        });
-    }
-    createPoseLandmarker();
-    /********************************************************************
-    // Demo 2: Continuously grab image from webcam stream and detect it.
-    ********************************************************************/
-    const video = document.getElementById("webcam");
-    const canvasElement = document.getElementById("output_canvas");
-    const canvasCtx = canvasElement.getContext("2d");
-    const drawingUtils = new DrawingUtils(canvasCtx);
-    // Check if webcam access is supported.
-    const hasGetUserMedia = () => { var _a; return !!((_a = navigator.mediaDevices) === null || _a === void 0 ? void 0 : _a.getUserMedia); };
-    // If webcam supported, add event listener to button for when user
-    // wants to activate it.
-    if (hasGetUserMedia()) {
-        enableCam;
-    }
-    else {
-        console.warn("getUserMedia() is not supported by your browser");
-    }
-    // Enable the live webcam view and start detection.
-    function enableCam() {
-        if (!poseLandmarker) {
-            console.log("Wait! poseLandmaker not loaded yet.");
-            return;
-        }
-        // getUsermedia parameters.
-        const constraints = {
-            video: true,
-        };
-        // Activate the webcam stream.
-        navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
-            video.srcObject = stream;
-            video.addEventListener("loadeddata", predictWebcam);
-        });
-    }
-    let lastVideoTime = -1;
-    function predictWebcam() {
-        return __awaiter(this, void 0, void 0, function* () {
-            canvasElement.style.height = videoHeight;
-            video.style.height = videoHeight;
-            canvasElement.style.width = videoWidth;
-            video.style.width = videoWidth;
-            // Now let's start detecting the stream.
-            yield poseLandmarker.setOptions({ runningMode: "VIDEO" });
-            let startTimeMs = performance.now();
-            if (lastVideoTime !== video.currentTime) {
-                lastVideoTime = video.currentTime;
-                poseLandmarker.detectForVideo(video, startTimeMs, (result) => {
-                    canvasCtx === null || canvasCtx === void 0 ? void 0 : canvasCtx.save();
-                    canvasCtx === null || canvasCtx === void 0 ? void 0 : canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-                    for (const landmark of result.landmarks) {
-                        drawingUtils.drawLandmarks(landmark, {
-                            radius: (data) => DrawingUtils.lerp(data.from.z, -0.15, 0.1, 5, 1),
-                        });
-                        drawingUtils.drawConnectors(landmark, PoseLandmarker.POSE_CONNECTIONS);
-                    }
-                    canvasCtx === null || canvasCtx === void 0 ? void 0 : canvasCtx.restore();
-                });
-            }
-            // Call this function again to keep predicting when the browser is ready.
-            if (webcamRunning === true) {
-                window.requestAnimationFrame(predictWebcam);
-            }
-        });
-    }
+
+function zColor(data) {
+  const z = clamp(data.from.z + 0.5, 0, 1);
+  return `rgba(0, ${255 * z}, ${255 * (1 - z)}, 1)`;
+}
+
+function onResultsPose(results) {
+  document.body.classList.add("loaded");
+  fpsControl.tick();
+  canvasCtx5.save();
+  canvasCtx5.clearRect(0, 0, out5.width, out5.height);
+  canvasCtx5.drawImage(results.image, 0, 0, out5.width, out5.height);
+  drawConnectors(canvasCtx5, results.poseLandmarks, POSE_CONNECTIONS, {
+    color: (data) => {
+      const x0 = out5.width * data.from.x;
+      const y0 = out5.height * data.from.y;
+      const x1 = out5.width * data.to.x;
+      const y1 = out5.height * data.to.y;
+
+      const z0 = clamp(data.from.z + 0.5, 0, 1);
+      const z1 = clamp(data.to.z + 0.5, 0, 1);
+
+      const gradient = canvasCtx5.createLinearGradient(x0, y0, x1, y1);
+      gradient.addColorStop(0, `rgba(0, ${255 * z0}, ${255 * (1 - z0)}, 1)`);
+      gradient.addColorStop(1.0, `rgba(0, ${255 * z1}, ${255 * (1 - z1)}, 1)`);
+      return gradient;
+    },
+  });
+  drawLandmarks(
+    canvasCtx5,
+    Object.values(POSE_LANDMARKS_LEFT).map(
+      (index) => results.poseLandmarks[index]
+    ),
+    { color: zColor, fillColor: "#FF0000" }
+  );
+  drawLandmarks(
+    canvasCtx5,
+    Object.values(POSE_LANDMARKS_RIGHT).map(
+      (index) => results.poseLandmarks[index]
+    ),
+    { color: zColor, fillColor: "#00FF00" }
+  );
+  drawLandmarks(
+    canvasCtx5,
+    Object.values(POSE_LANDMARKS_NEUTRAL).map(
+      (index) => results.poseLandmarks[index]
+    ),
+    { color: zColor, fillColor: "#AAAAAA" }
+  );
+  canvasCtx5.restore();
+}
+
+const pose = new Pose({
+  locateFile: (file) => {
+    return `https://cdn.jsdelivr.net/npm/@mediapipe/pose@0.2/${file}`;
+  },
 });
+pose.onResults(onResultsPose);
+
+const camera = new Camera(video5, {
+  onFrame: async () => {
+    await pose.send({ image: video5 });
+  },
+  width: 480,
+  height: 480,
+});
+camera.start();
+
+new ControlPanel(controlsElement5, {
+  selfieMode: true,
+  upperBodyOnly: false,
+  smoothLandmarks: true,
+  minDetectionConfidence: 0.5,
+  minTrackingConfidence: 0.5,
+})
+  .add([
+    new StaticText({ title: "MediaPipe Pose" }),
+    fpsControl,
+    new Toggle({ title: "Selfie Mode", field: "selfieMode" }),
+    new Toggle({ title: "Upper-body Only", field: "upperBodyOnly" }),
+    new Toggle({ title: "Smooth Landmarks", field: "smoothLandmarks" }),
+    new Slider({
+      title: "Min Detection Confidence",
+      field: "minDetectionConfidence",
+      range: [0, 1],
+      step: 0.01,
+    }),
+    new Slider({
+      title: "Min Tracking Confidence",
+      field: "minTrackingConfidence",
+      range: [0, 1],
+      step: 0.01,
+    }),
+  ])
+  .on((options) => {
+    video5.classList.toggle("selfie", options.selfieMode);
+    pose.setOptions(options);
+  });
